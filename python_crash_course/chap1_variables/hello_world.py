1db9a3e70f459483b46d9089a5bf83478c0a75b3
Daniel John Idorot <ido20002@byui.edu>
2022-05-28 21:03:50 +0800

    initial commit

---
+++ python_crash_course/chap1_variables/hello_world.py
@@ -0,0 +1,28 @@
+
+print("Hello Python world!")
+
+# Variables 
+message = "Hello Python world!"
+
+print(message)
+
+
+message = "Hello Python Crash Course World!"
+
+print(message)
+
+
+# VARIABLES > Naming and Using Variables
+
+# VARIABLES > Avoiding Name Errors When Using Variables
+
+mesage = "Hello Python Crash Course World!"
+
+print(mesage)
+
+
+# VARIABLES > Variables Are Labels
+
+
+
+
---
+++ python_crash_course/chap1_variables/name.py
@@ -0,0 +1,15 @@
+
+# STRINGS > Changing Case in a String with Methods
+
+name = "ada lovelace"
+
+print(name.title())
+
+
+name = "Ada Lovelace"
+
+print(name.upper())
+
+print(name.lower())
+
+
