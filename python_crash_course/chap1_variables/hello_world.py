0f8a655b7b6a176ff69094b47f07fc2dfe52b7a2
Daniel John Idorot <ido20002@byui.edu>
2022-05-31 22:59:06 +0800

    initial commit

--- python_crash_course/chap1_variables/hello_world.py
+++ python_crash_course/chap1_variables/hello_world.py
@@ -1,27 +1,60 @@
 
-print("Hello Python world!")
+# print("Hello Python world!")
 
-# Variables 
-message = "Hello Python world!"
+# # Variables 
+# message = "Hello Python world!"
 
-print(message)
-
-
-message = "Hello Python Crash Course World!"
-
-print(message)
+# print(message)
 
 
-# VARIABLES > Naming and Using Variables
+# message = "Hello Python Crash Course World!"
 
-# VARIABLES > Avoiding Name Errors When Using Variables
+# print(message)
 
-mesage = "Hello Python Crash Course World!"
 
-print(mesage)
+# # VARIABLES > Naming and Using Variables
+
+# # VARIABLES > Avoiding Name Errors When Using Variables
+
+# mesage = "Hello Python Crash Course World!"
+
+# print(mesage)
 
 
 # VARIABLES > Variables Are Labels
+
+first_name = "ada"
+
+last_name = "lovelace"
+
+full_name = f"{first_name} {last_name}"
+
+message = f"Hello, {full_name.title()}!"
+
+print(message)
+
+
+full_name = "{} {}".format(first_name, last_name)
+
+# VARIABLES > Adding Whitespace to Strings with Tabs or Newlines
+
+print("Python")
+   
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavaScript")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+# VARIABLES > Stripping Whitespace
+
+favorite_language = 'python '
+
+favorite_language
+
+favorite_language.rstrip()
+   
+favorite_language
 
 
 
