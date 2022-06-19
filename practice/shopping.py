48e37816d26875f182d7e9dc79907b4047d847d0
Daniel John Idorot <ido20002@byui.edu>
2022-06-19 21:30:02 +0800

    initial commit

--- practice/shopping.py
+++ practice/shopping.py
@@ -1,18 +1,24 @@
-# Shopping Cart
+print(" Welcome to the Shopping Cart Program! \n")
+action = int(input("Please select one of the following: \n\n 1. Add Item \n 2. View cart \n 3. Remove item \n 4. Compute total \n 5. Quit \n \nPlease enter an action: "))
+# print(f"{menu} \n")
+action_list = [0, 1, 2, 3, 4, 5]
+list_items = []
+# print(len(action_list))
 
-# empty items and prices
-items = []
-prices = []
+while action != len(action_list):
 
-# Type item
-new_item = input("What item would you like to add? ")
+    if action == action_list[1]:
+        add_item = input("What item would you like to add? ")
+        list_items.append(add_item)
+        print(f"{add_item} has been added to the cart.")
+        break
 
-# Tye price
-price = float(input(f"What is the price of {new_item}? "))
+    elif action == action_list[2]:
+        print(list_items)
+        break
 
-# Add item and price
-items.append(new_item)
-prices.append(price)
+    elif action == action_list[3]:
+        break
 
-print(f"The price of this {items} is {prices}\n")
-print(f"{items} has been added to the cart\n")
+    else:
+        print("Incorrect input. Try again")
